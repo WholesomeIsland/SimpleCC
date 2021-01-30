3600906b4ba0d5d892bd981ca8c21ff94236782e
Ty Alex <spinnerbowl@hotmail.com>
2021-01-30 15:37:56 -0500

    added -h option

--- scc.cpp
+++ scc.cpp
@@ -4,12 +4,21 @@
 #include "SimpleCToC.h"
 
 using namespace std;
-
+ string helpstr = R"(Welcome to scc, the simpleC transpiler.
+flags:
+	-h,--help display this message.
+usage:
+	scc [inputfile] [outputfile]
+ )";
 int main(int argc, char** argv)
 {
+	if (argv[1] == "-h" || argv[1] == "--help")
+	{
+		cout << helpstr << endl;
+	}
 	if (argc != 3)
 	{
-		std::cout << "scc: Error, 2 arguments only." << std::endl;
+		std::cout << "scc: Error, Invalid number of arguments." << std::endl;
 		return 1;
 	}
 	ofstream outputfile(argv[2]);
